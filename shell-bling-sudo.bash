c49de9c057f299d298364083d87d8bd11e98893b
Andrew Quinn <53230903+hiAndrewQuinn@users.noreply.github.com>
2023-10-21 11:19:47 +0300

    refactor: Separate out all apt installs

--- shell-bling-sudo.bash
+++ shell-bling-sudo.bash
@@ -20,29 +20,29 @@ add-apt-repository -y ppa:neovim-ppa/unstable
 
 apt update -y
 
-apt install -y fish \
-	curl \
-	git \
-	micro \
-	ripgrep \
-	jq \
-	vim \
-	tmux \
-	neovim \
-	tree \
-	htop \
-	bat \
-	fd-find \
-	kitty \
-	lnav \
-	gron \
-	csvkit \
-	entr \
-	xclip \
-	gcc \
-        g++ \
-	make \
-        nodejs
+apt install -y fish && apt -y clean
+apt install -y curl && apt -y clean
+apt install -y git && apt -y clean
+apt install -y micro && apt -y clean
+apt install -y ripgrep && apt -y clean
+apt install -y jq && apt -y clean
+apt install -y vim && apt -y clean
+apt install -y tmux && apt -y clean
+apt install -y neovim && apt -y clean
+apt install -y tree && apt -y clean
+apt install -y htop && apt -y clean
+apt install -y bat && apt -y clean
+apt install -y fd-find && apt -y clean
+apt install -y kitty && apt -y clean
+apt install -y lnav && apt -y clean
+apt install -y gron && apt -y clean
+apt install -y csvkit && apt -y clean
+apt install -y entr && apt -y clean
+apt install -y xclip && apt -y clean
+apt install -y gcc && apt -y clean
+apt install -y g++ && apt -y clean
+apt install -y make && apt -y clean
+apt install -y nodejs && apt -y clean
 
 snap install tldr \
 	cheat \
